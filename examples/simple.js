1b46546c245194e327d24e3c17dcdbed26542ccd
Anders Bakken <agbakken@gmail.com>
2014-05-14 01:30:41 -0700

    More example stuff.

--- examples/simple.js
+++ examples/simple.js
@@ -1,27 +1,17 @@
-// var nrdp = {
-//     device: {
-//         get ESN() { return "ESN"; }
-//     }
-// };
+var castclient = {
+    init: function () {
+        function initTimeout()
+        {
 
-// var foo = nrdp.device.ESN;
 
-// function foo()
-// {
-//     var aaaaaaaaaaaa = 1;
-//     ++aaaaaaaaaaaa;
-// }
 
-var aaaaaaaaaaaa;
-function bar()
-{
-    aaaaaaaaaaaa = 1;
-    ++aaaaaaaaaaaa;
-    var b = aaaaaaaaaaaa;
-}
 
-// function balle()
-// {
-//     var fisk = true;
-//     fisk = false;
-// }
+
+            restartInitTimer();
+        }
+        function restartInitTimer()
+        {
+        }
+        // restartInitTimer();
+    }
+};
